091875e40385184cce008925e3105d28d910503f
Leo Liu <karta1041337@gmail.com>
2022-02-26 00:04:03 +0800

    addd

--- Formula/aria2-service.rb
+++ Formula/aria2-service.rb
@@ -4,6 +4,7 @@ class Aria2Service < Formula
   homepage "https://aria2.github.io/"
   url "https://example.com"
   sha256 "ea8fac7c65fb589b0d53560f5251f74f9e9b243478dcb6b3ea79b5e36449c8d9"
+  depends_on "aria2"
 
   def install
     system "touch", "brew-keep"
@@ -11,7 +12,7 @@ class Aria2Service < Formula
   end
 
   service do
-    run [opt_bin/"aria2c", "--conf", etc/"aria2.conf"]
+    run [HOMEBREW_PREFIX/"bin/aria2c", "--conf", etc/"aria2.conf"]
     keep_alive true
     working_dir HOMEBREW_PREFIX
     log_path var/"log/aria2.log"
