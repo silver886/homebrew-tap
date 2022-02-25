e1d1c09384aa2d9b04d276b23e4afb15d41eb51e
Leo Liu <karta1041337@gmail.com>
2022-02-25 23:14:25 +0800

    Implement service

---
+++ Formula/aria2-service.rb
@@ -0,0 +1,15 @@
+class Aria2Service < Formula
+  desc "Service for aria2"
+  version "0.0.1"
+  homepage "https://aria2.github.io/"
+  url "https://example.com"
+  sha256 "ea8fac7c65fb589b0d53560f5251f74f9e9b243478dcb6b3ea79b5e36449c8d9"
+
+  service do
+    run [opt_bin/"aria2c", "--conf", etc/"aria2.conf"]
+    keep_alive true
+    working_dir HOMEBREW_PREFIX
+    log_path var/"log/aria2.log"
+    error_log_path var/"log/aria2.log"
+  end
+end
