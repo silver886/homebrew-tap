3665245677f156fa1c971c1ff1f23489d99220cd
Leo Liu <karta1041337@gmail.com>
2022-02-26 11:24:59 +0800

    100

--- Formula/aria2-service.rb
+++ Formula/aria2-service.rb
@@ -1,6 +1,6 @@
 class Aria2Service < Formula
   desc "Service for aria2"
-  version "0.0.1"
+  version "1.0.0"
   homepage "https://aria2.github.io/"
   url "https://example.com"
   sha256 "ea8fac7c65fb589b0d53560f5251f74f9e9b243478dcb6b3ea79b5e36449c8d9"
