e01744d12b941cf7fc0d13aff0b85390ce4c6eb5
Leo Liu <silver886@users.noreply.github.com>
2022-02-26 11:26:09 +0800

    Add aria2 service (#3)
    * Implement service
    
    * Add install
    
    * Add log
    
    * fix?
    
    * remove?
    
    * true
    
    * bupd
    
    * don't use dot file
    
    * addd
    
    * remove home
    
    * add home
    
    * try
    
    * fix
    
    * tt
    
    * config
    
    * try
    
    * ruby
    
    * try ruby
    
    * yo
    
    * caveats
    
    * extract?
    
    * var?
    
    * glo?
    
    * str?
    
    * re
    
    * oops
    
    * nah
    
    * eos
    
    * ??
    
    * yooo
    
    * rm
    
    * hoooooo
    
    * add
    
    * read
    
    * oh
    
    * ur
    
    * oh
    
    * in adven
    
    * unp
    
    * df
    
    * 64!
    
    * 100

---
+++ Formula/aria2-service.rb
@@ -0,0 +1,106 @@
+class Aria2Service < Formula
+  desc "Service for aria2"
+  version "1.0.0"
+  homepage "https://aria2.github.io/"
+  url "https://example.com"
+  sha256 "ea8fac7c65fb589b0d53560f5251f74f9e9b243478dcb6b3ea79b5e36449c8d9"
+  depends_on "aria2"
+
+  def install
+    pass = ""
+    if File.file?(etc/"aria2.conf")
+      File.open(etc/"aria2.conf").each do |line|
+        if line.include?('rpc-secret=')
+          pass = line[11..-1]
+        end
+      end
+      rm etc/"aria2.conf"
+    else
+      s = [('0'..'9'), ('A'..'Z'), ('a'..'z')].map(&:to_a).flatten
+      pass = "#{64.times.map { s[rand(s.length)] }.join}"
+    end
+    File.open('aria2.conf', 'w') { |file| file.write(<<~EOS.strip
+      ###########
+      # General #
+      ###########
+
+      dir=${HOME}/Downloads
+
+      input-file=#{etc}/aria2.session
+      save-session=#{etc}/aria2.session
+      save-session-interval=60
+
+      console-log-level=warn
+
+      disk-cache=64M
+      file-allocation=falloc
+      enable-mmap=true
+      content-disposition-default-utf8=true
+
+      continue=true
+      max-concurrent-downloads=8
+
+      ##################
+      # HTTP(S)/(S)FTP #
+      ##################
+
+      max-connection-per-server=16
+      split=16
+      min-split-size=8M
+      stream-piece-selector=geom
+
+      ###########
+      # HTTP(S) #
+      ###########
+
+      user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:97.0) Gecko/20100101 Firefox/97.0
+      referer=*
+      http-no-cache=true
+
+      ##############
+      # BitTorrent #
+      ##############
+
+      follow-torrent=true
+      seed-time=0
+      listen-port=51413
+
+      enable-dht=true
+      enable-dht6=true
+      dht-listen-port=6969
+
+      bt-max-peers=0
+      enable-peer-exchange=true
+      peer-id-prefix=-TR3000-
+      peer-agent=Transmission/3.00
+
+      bt-hash-check-seed=true
+      bt-seed-unverified=true
+      bt-save-metadata=true
+
+      #######
+      # RPC #
+      #######
+
+      enable-rpc=true
+      rpc-listen-port=6800
+      rpc-listen-all=false
+      rpc-allow-origin-all=true
+      rpc-secret=#{pass}
+      EOS
+    ) }
+    system "touch", "brew-keep", "aria2.session"
+    etc.install "aria2.conf"
+    etc.install "aria2.session"
+    prefix.install "brew-keep"
+    puts "RPC secret is: #{pass}"
+  end
+
+  service do
+    run [HOMEBREW_PREFIX/"bin/aria2c", "--conf", etc/"aria2.conf"]
+    keep_alive true
+    working_dir HOMEBREW_PREFIX
+    log_path var/"log/aria2.log"
+    error_log_path var/"log/aria2.log"
+  end
+end
