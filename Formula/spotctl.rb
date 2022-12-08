0aa17336c45593c1a4ffb6a461c4675be227a150
Leo Liu <silver886@users.noreply.github.com>
2022-12-09 00:33:31 +0800

    Update spotctl URL (#13)

--- Formula/spotctl.rb
+++ Formula/spotctl.rb
@@ -3,7 +3,7 @@ class Spotctl < Formula
   homepage "https://github.com/jingweno/spotctl"
   version "1.0.1"
   sha256 "a0276bd0c0fb65e7b24885f17d3547276dcf9b059b9507abe51edfa5f5388f89"
-  url "https://github.com/jingweno/spotctl/releases/download/v1.0.1/darwin-amd64-1.0.1.tar.gz"
+  url "https://github.com/jingweno/spotctl/releases/download/v#{version}/darwin-amd64-#{version}.tar.gz"
 
   def install
     bin.install "spotctl"
