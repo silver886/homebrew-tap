5478d85939cb456681c0abd175b25fa103559947
silver886 <silver886@users.noreply.github.com>
2022-12-27 01:40:51 +0000

    Update on 2022-12-27

--- Casks/dupeguru-reference-modifier.rb
+++ Casks/dupeguru-reference-modifier.rb
@@ -1,12 +1,12 @@
 cask "dupeguru-reference-modifier" do
-  version "0.2.2"
-  sha256 "81fa50f487a5fa6f5d5ed7be062c47cfaf1116e242fb225fad1705163a4c3431"
+  version "0.3.0"
+  sha256 "c5ec47e3fc94bae1a39237a665fc6dcdb4d20eaa279bf0fd8cdbd2f3598642cc"
 
   url "https://github.com/silver886/dupeguru-reference-modifier/releases/download/v#{version}/dupeGuru.Reference.Modifier-amd64.zip"
-  name "dupeGuru Reference Modifier"
-  desc "A quicker way to change the reference file in dupeGuru by name."
-  homepage "https://github.com/silver886/dupeguru-reference-modifier"
   appcast "https://github.com/silver886/dupeguru-reference-modifier/releases.atom"
+  name "dupeGuru Reference Modifier"
+  desc "Quicker way to change the reference file in dupeGuru by name"
+  homepage "https://github.com/silver886/dupeguru-reference-modifier"
 
   app "dupeGuru Reference Modifier.app"
 
