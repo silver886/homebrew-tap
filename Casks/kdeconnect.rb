a91c72a70edba453e0283c0e56d12da6e98d5636
Leo Liu <silver886@users.noreply.github.com>
2022-12-29 17:59:33 +0800

    Add KDE Connect (#22)
    Add `livecheck` with fixed `version` and `sha256`.
    
    Reference:
    https://invent.kde.org/packaging/homebrew-kde/-/blob/master/Casks/kdeconnect.rb

---
+++ Casks/kdeconnect.rb
@@ -0,0 +1,30 @@
+cask "kdeconnect" do
+  version "22.12.0-1169"
+  sha256 "44a526b4e2c109ba58bf038c51773c7298a612b4962ac881a8c9d2f851ec2ccd"
+
+  url "https://binary-factory.kde.org/view/MacOS/job/kdeconnect-kde_Release_macos/lastStableBuild/artifact/kdeconnect-kde-#{version}-macos-clang-x86_64.dmg"
+  name "KDE Connect"
+  desc "Enabling communication between all your devices"
+  homepage "https://kdeconnect.kde.org/"
+
+  livecheck do
+    url "https://binary-factory.kde.org/view/MacOS/job/kdeconnect-kde_Release_macos/lastStableBuild/artifact/"
+    regex(/href=.*?kdeconnect-kde[._-]v?(\d+(?:[.-]\d+)+)-macos-clang-x86_64\.dmg/i)
+  end
+
+  app "kdeconnect-indicator.app", target: "KDE Connect.app"
+  binary "#{appdir}/KDE Connect.app/Contents/MacOS/kdeconnect-cli",
+         target: "kdeconnect"
+
+  uninstall quit: "org.kde.kdeconnect"
+
+  zap trash: [
+    "~/Library/Application Support/kpeoplevcard",
+    "~/Library/Caches/kdeconnect.sms",
+    "~/Library/Preferences/kdeconnect",
+    "~/Library/Preferences/kdeconnect_runcommand",
+    "~/Library/Preferences/kdeconnect_sendnotifications",
+    "~/Library/Preferences/kdeconnect_share",
+    "~/Library/Preferences/org.kde.kdeconnect.plist",
+  ]
+end
