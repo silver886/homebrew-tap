cffc6aa4dfb676c669d2e9fdbceaa4bab7643a19
Leo Liu <karta1041337@gmail.com>
2022-12-31 13:24:42 +0800

    Fix `upscayl`

--- Casks/upscayl.rb
+++ Casks/upscayl.rb
@@ -2,7 +2,7 @@ cask "upscayl" do
   version "1.5.5"
   sha256 "b59907c12db7cdcbe237bfae5818ef2bb43ec0bf15c3c7f0384dff4e82fc6984"
 
-  url "https://github.com/upscayl/upscayl/releases/download/v#{version}/Upscayl-#{version}.dmg",
+  url "https://github.com/upscayl/upscayl/releases/download/v#{version}/Upscayl-#{version}-mac.dmg",
       verified: "github.com/upscayl/upscayl/"
   appcast "https://github.com/upscayl/upscayl/releases.atom"
   name "Upscayl"
