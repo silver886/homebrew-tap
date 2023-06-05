7cfe82e4b9e87fe7ca8ea52470dadedc7b243966
silver886 <silver886@users.noreply.github.com>
2023-06-05 01:56:04 +0000

    Update on 2023-06-05

--- Casks/kdeconnect-nightly.rb
+++ Casks/kdeconnect-nightly.rb
@@ -1,6 +1,6 @@
 cask "kdeconnect-nightly" do
-  version "23.04.1-1323"
-  sha256 "e40e857353633da4ef9218f036f98cda88a7c11ad5efb8b1cf772fd91a16214f"
+  version "23.04.1-1324"
+  sha256 "0dc644d60f1301a55f73919c3b020cbd7cdfacdfa1d3f7a333b6bca155da652b"
 
   url "https://binary-factory.kde.org/view/MacOS/job/kdeconnect-kde_Release_macos/#{version.sub(/.*-/, "")}/artifact/kdeconnect-kde-#{version}-macos-clang-x86_64.dmg"
   name "KDE Connect"
