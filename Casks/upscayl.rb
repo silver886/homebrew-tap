6ba34a772ddf93c3f54e0912474bc6916798c24f
L <11664880+silver886@users.noreply.github.com>
2026-01-31 20:21:14 -0800

    Update upscayl
    Update homepage
    
    asdf

--- Casks/upscayl.rb
+++ Casks/upscayl.rb
@@ -6,12 +6,14 @@ cask "upscayl" do
       verified: "github.com/upscayl/upscayl/"
   name "Upscayl"
   desc "Free and Open Source AI Image Upscaler"
-  homepage "https://upscayl.github.io/"
+  homepage "https://upscayl.org/"
 
   livecheck do
     url :url
     strategy :github_latest
   end
+
+  depends_on macos: ">= :monterey"
 
   app "Upscayl.app"
 
