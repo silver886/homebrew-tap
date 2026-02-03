3c92aa2921656e951d3656802fd972e91e2579d5
silver886 <11664880+silver886@users.noreply.github.com>
2026-02-03 02:22:12 +0000

    Update on 2026-02-03

--- Casks/kdeconnect-nightly.rb
+++ Casks/kdeconnect-nightly.rb
@@ -1,15 +1,17 @@
 cask "kdeconnect-nightly" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "23.04.3-1375"
+  version "5850"
 
   on_arm do
+    sha256 "52d8cc8a04905ffb1a5c35ec3173c269f02c53be90b0b16ebff78bd495e647c8"
+
     url "https://cdn.kde.org/ci-builds/network/kdeconnect-kde/master/macos-arm64/kdeconnect-kde-master-#{version}-macos-clang-arm64.dmg"
-    sha256 "e37b5f8b8995a0ecc41ca7fcae90d79bcd652b7a25d2f6e52c4e2e79aef7fec1"
   end
   on_intel do
+    sha256 "833ac3bfda3e9320bc825e91cf49c6f13e0b62d77a56449459ae63806b0c38fc"
+
     url "https://cdn.kde.org/ci-builds/network/kdeconnect-kde/master/macos-x86_64/kdeconnect-kde-master-#{version}-macos-clang-x86_64.dmg"
-    sha256 "e97de6804d8811dd2f1bc45d709d87fb6fe45963aae710c24a4ed655ecd8eb8a"
   end
 
   name "KDE Connect"
